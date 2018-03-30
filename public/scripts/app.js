8b236b2c55d526f282888026906c62e0389cc3f8
git3sh <giteshkmr52@gmail.com>
2018-03-30 13:33:18 +0530

    inital commit

---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _validator = require('validator');
+
+var _validator2 = _interopRequireDefault(_validator);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log(_validator2.default.isEmail('test')); //import './utils.js';
+//import {sque ,add} from './utils.js';
+//console.log(sque(4));
+
+//console.log(add(4 , 6));
+//console.log('dflkdslfsdlhfjdhsfhdshfdskfu');
